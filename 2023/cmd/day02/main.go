42ff4cbf6089a4ee59b8a2838083f19a05d87c3b
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2023-12-02 11:25:40 +0100

    Add solution for day 2.2

--- 2023/cmd/day02/main.go
+++ 2023/cmd/day02/main.go
@@ -9,56 +9,39 @@ import (
 	"github.com/eotsn/advent_of_code/2023/file"
 )
 
-var cubes = map[string]int{
-	"red":   12,
-	"green": 13,
-	"blue":  14,
-}
-
 func main() {
 	lines, err := file.ReadLines("input/02.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	games := make(map[int]bool)
+	var powerSum int
 	for _, line := range lines {
 		var sc scanner.Scanner
 		sc.Init(strings.NewReader(line))
 
-		var game int
+		cubes := make(map[string]int) // defaults all values to 0
+
 		var prevToken string
-	parse:
 		for token := sc.Scan(); token != scanner.EOF; token = sc.Scan() {
 			text := sc.TokenText()
 
-			switch token {
-			case scanner.Ident:
-				switch text {
-				case "red", "green", "blue":
-					count, _ := strconv.Atoi(prevToken)
-					if count > cubes[text] {
-						games[game] = false
-						break parse
-					}
-				default:
+			switch text {
+			case "red", "green", "blue":
+				count, _ := strconv.Atoi(prevToken)
+				if count > cubes[text] {
+					cubes[text] = count
 				}
-			case scanner.Int:
-				if prevToken == "Game" {
-					game, _ = strconv.Atoi(text)
-					games[game] = true // assume game is valid
-				}
-			default:
 			}
 			prevToken = text
 		}
+
+		sum := 1
+		for _, count := range cubes {
+			sum *= count // this will break if we have a game where any color is never picked
+		}
+		powerSum += sum
 	}
 
-	var sum int
-	for game, valid := range games {
-		if valid {
-			sum += game
-		}
-	}
-	fmt.Println(sum)
+	fmt.Println(powerSum)
 }
