41e53e4e9c1c2d5d737a3d7ba4911f693fbef85f
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2023-12-03 13:23:42 +0100

    Add solution for day 3.2

--- 2023/cmd/day03/main.go
+++ 2023/cmd/day03/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"unicode"
 
 	"github.com/eotsn/advent_of_code/2023/file"
 )
+
+type Gear struct {
+	x, y int
+}
+
+type PartNo struct {
+	value int
+	x0    int
+	x1    int
+}
 
 func main() {
 	lines, err := file.ReadLines("input/03.txt")
@@ -14,55 +25,50 @@ func main() {
 		panic(err)
 	}
 
-	symbols := make(map[int][]int) // store symbol indices for each line
+	// add some padding so we can skip edge cases for the 1st and last lines
+	var padding string
+	for i := 0; i < len(lines[0]); i++ {
+		padding += "."
+	}
+	lines = append([]string{padding}, lines...)
+	lines = append(lines, padding)
+
+	var gears []Gear
+	parts := make([][]PartNo, len(lines)) // store part numbers for each line
 	for i, line := range lines {
 		for j, rune := range line {
-			if !unicode.IsDigit(rune) && rune != '.' {
-				symbols[i] = append(symbols[i], j)
+			if !unicode.IsDigit(rune) && rune == '*' {
+				gears = append(gears, Gear{y: i, x: j})
 			}
+		}
+
+		r := regexp.MustCompile(`(\d+)`)
+		matches := r.FindAllStringSubmatchIndex(line, -1)
+		for _, match := range matches {
+			partNo, _ := strconv.Atoi(line[match[0]:match[1]])
+			parts[i] = append(parts[i], PartNo{
+				value: partNo,
+				x0:    match[0],
+				x1:    match[1],
+			})
 		}
 	}
 
 	var sum int
 
-	for i, line := range lines {
-		symbolIndices := symbols[i] // combine indices for surrounding lines
-		if i != 0 {
-			symbolIndices = append(symbolIndices, symbols[i-1]...)
-		}
-		if i != len(lines) {
-			symbolIndices = append(symbolIndices, symbols[i+1]...)
-		}
+	for _, gear := range gears {
+		var p []int
 
-		var partNo string
-
-		start := -1 // save the start index of the current part number
-		for j, rune := range line {
-			if unicode.IsDigit(rune) {
-				if start < 0 {
-					start = j
+		// collect the matching part numbers for the previous, current and next line
+		for _, y := range []int{gear.y - 1, gear.y + 1, gear.y} {
+			for _, part := range parts[y] {
+				if part.x0-1 <= gear.x && part.x1 >= gear.x {
+					p = append(p, part.value)
 				}
-				partNo += fmt.Sprintf("%c", rune)
-				if j == len(line)-1 { // make sure we catch part numbers at the end of the line
-					for _, symbolIndex := range symbolIndices {
-						if symbolIndex >= start-1 && symbolIndex <= j {
-							n, _ := strconv.Atoi(partNo)
-							sum += n
-						}
-					}
-					partNo = ""
-					start = -1
-				}
-			} else if start >= 0 {
-				for _, symbolIndex := range symbolIndices {
-					if symbolIndex >= start-1 && symbolIndex <= j {
-						n, _ := strconv.Atoi(partNo)
-						sum += n
-					}
-				}
-				partNo = ""
-				start = -1
 			}
+		}
+		if len(p) == 2 {
+			sum += p[0] * p[1]
 		}
 	}
 
