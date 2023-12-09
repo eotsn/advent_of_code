7952cb9069acd764ad7270720997ea35f54c2ca9
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2023-12-09 15:24:36 +0100

    Add solution for Day 8.2

--- 2023/cmd/day08/main.go
+++ 2023/cmd/day08/main.go
@@ -14,6 +14,8 @@ func main() {
 
 	instructions := lines[0]
 
+	var startNodes []string
+
 	nodes := make(map[string][]string)
 	for _, line := range lines[2:] { // skip first two lines
 		var (
@@ -21,27 +23,47 @@ func main() {
 			left  = line[7:10]
 			right = line[12:15]
 		)
+		if node[2] == 'A' {
+			startNodes = append(startNodes, node)
+		}
 		nodes[node] = []string{left, right}
 	}
 
-	var (
-		steps       = 0
-		currentNode = "AAA"
-	)
+	var steps []int
+	for _, node := range startNodes {
+		currentNode := node
 
-	var i = 0
-	for {
-		if currentNode == "ZZZ" {
-			fmt.Println(fmt.Sprintf("found ZZZ in %d steps", steps))
-			break
+		var i, j int
+		for {
+			if currentNode[2] == 'Z' {
+				steps = append(steps, j)
+				break
+			}
+			switch instructions[i] {
+			case 'L':
+				currentNode = nodes[currentNode][0]
+			case 'R':
+				currentNode = nodes[currentNode][1]
+			}
+			j++
+			i = (i + 1) % len(instructions)
 		}
-		switch instructions[i] {
-		case 'L':
-			currentNode = nodes[currentNode][0]
-		case 'R':
-			currentNode = nodes[currentNode][1]
-		}
-		steps++
-		i = (i + 1) % len(instructions)
 	}
+
+	fmt.Println(lcm(steps[0], steps[1], steps[2:]...))
+}
+
+func lcm(a, b int, integers ...int) int {
+	result := a * b / gcd(a, b)
+	for _, i := range integers {
+		result = lcm(result, i)
+	}
+	return result
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
